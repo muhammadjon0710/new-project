2e1986ff5d9b9da70e7575ef2ea7f2e1c8e9fc84
muhammadjon0710 <baxodirurinov07@gmail.com>
2024-11-14 15:21:42 +0500

    salom

---
+++ index.js
@@ -0,0 +1,382 @@
+1
+
+
+// function pubg(a) {
+//      let  = a * 2;
+//     console.log(let);
+// }
+
+// pubg(5);
+
+
+2
+
+// function dog(b) {
+//     if (b % 2 == 0) {
+//         console.log("juft son");
+//     } else {
+//         console.log("toq son");
+//     }
+// }
+
+// dog(4); 
+// dog(5);
+
+3
+
+
+// function blue(w1, q2, r3) {
+//     return w1 + q2 + r3;
+// }
+// const result = blue(2, 4, 6);
+// console.log(result); 
+
+
+4
+
+
+// function dog(fotter) {
+//     return fotter % 10;
+// }
+
+// const result = dog(25);
+// console.log(result);
+
+5
+
+// function www(h1, h2) {
+//     return h1 > h2 ? h1 : h2;
+// }
+
+
+// const result = www(10, 20);
+// console.log(result);
+
+6
+
+// function red(g) {
+//     if (g > 0) {
+//         console.log("musbat");
+//     } else {
+//         console.log("musbat emas");
+//     }
+// }
+// red(5);
+// red(-3); 
+
+
+
+7
+
+// function squareNumber(tok) {
+//     return tok * tok
+// }
+
+// const result = squareNumber(5);
+// console.log(result);
+
+
+8
+
+
+// function multiplyNumbers(k1, k2) {
+//     return k1 * k2; 
+// }
+
+// const result = multiplyNumbers(2, 3);
+// console.log(result);
+
+
+9
+
+
+// function isEqual(m1, m2) {
+//     if (m1 == m2) {
+//         console.log("teng");
+//     } else {
+//         console.log("teng emas");
+//     }
+// }
+
+// isEqual(5, 5); 
+// isEqual(3, 9);
+
+
+11
+
+// function incrementNumber(i) {
+//     return i + 1;
+// }
+
+
+// const result = incrementNumber(109);
+// console.log(result); 
+
+
+12
+
+// function isNegative(eee) {
+//     if (eee < 0) {
+//         console.log("manfiy");
+//     } else {
+//         console.log("manfiy emas");
+//     }
+// }
+
+
+
+// isNegative(-3); 
+// isNegatiFve(4);  
+
+
+13
+
+
+// function divideByHundred(we) {
+//     return we / 100;
+// }
+
+
+// const result = divideByHundred(490);
+// console.log(result); 
+
+
+14
+
+// function isZero(er) {
+//     if (er == 0) {
+//         console.log("nol");
+//     } else {
+//         console.log("nol emas");
+//     }
+// }
+
+
+// isZero(13124);  
+// isZero(0);   
+
+
+15
+
+// function subtractNumbers(ert1, ert2) {
+//     return ert1 - ert2;
+// }
+
+// const result = subtractNumbers(13, 5);
+// console.log(result); 
+
+
+
+// let b  = prompt("soni kiriting");
+
+
+// function inter(b) {
+//     if (b > 0 ) {
+//         console.log(b * -1);
+
+//     } else {
+//         console.log(b);
+
+//     }
+// }
+
+
+// inter(-8);
+
+
+
+// function hokey(a, b) {
+//     if (a < 0 && b > 0) {
+//         console.log("To'g'ri");
+
+//     } else {
+//         console.log("No to'g'ri");
+
+//     }
+// }
+
+// hokey(-3, 23);
+
+
+
+123124
+
+
+
+// function yt(a) {
+//         console.log(2024 - a);
+// }
+
+// yt(1800 );
+
+// let a = prompt("soni kiriting");
+
+
+
+
+
+
+// function rtrtr(a) {
+//     if (a <= 50) {
+//         console.log(2024 - a);
+//     } else {
+//         console.log(2024 - a);
+//     }
+// }
+
+// rtrtr(100);
+
+
+
+// function ttr(a, b, c) {
+//     console.log(Math.pow(a,3));
+//     console.log(Math.pow(b,3));
+//     console.log(Math.pow(c,3));
+// }
+
+// ttr(3, 6, 9);
+
+
+
+
+// function key(a, b, c) {
+//     console.log(Math.pow(a, 2));
+//     console.log(Math.pow(b, 2));
+//     console.log(Math.pow(c, 2));
+//     console.log(Math.pow(a, 3));
+//     console.log(Math.pow(b, 3));
+//     console.log(Math.pow(c, 3));
+//     console.log(Math.pow(a, 4));
+//     console.log(Math.pow(b, 4));
+//     console.log(Math.pow(c, 4));
+// }
+// key(2, 3, 6);
+
+
+
+// let number = +prompt("ixtiyoriy soni kiriting");
+
+
+
+// function divideByHundred() {
+//     if (number > 0 && number < 18) {
+//         console.log("Soz 18 yoshdan kichik siz");
+//     } else if (number > 18 && number > 18) {
+//         console.log("Web saytmizga xosh kelibsiz");
+//     }
+//     else {
+//         console.log("Erorr");
+//     }
+// }
+// divideByHundred();
+
+
+// const isNegatiFve = function (a, b) {
+//     return a * b;
+// }
+// console.log(isNegatiFve(4, 7));
+
+
+213123123
+
+
+// let a = prompt("soni kiriting");
+
+// function divideByHundred(a, b, c) {
+//     if (a ) {
+
+//     } else {
+//     }
+// }
+
+
+
+// function matnUzunligi() {
+//     let a = prompt("ixtiyoriy matn kiriting: ");
+//     console.log("matn uzunligi: " + a.length + " ta belgi.");
+// }
+
+// matnUzunligi();
+
+
+
+// let ism =  "Muhammadjon";
+// let age = 14;
+// let contry = "Uzbekistan";
+// console.log(
+//     `salom mening ismim ${ism}. Mening yoshim ${age} da. Menimg vatanim ${contry}`
+// );
+
+
+
+
+// let son = 15;
+// for (let i = 0; i < son; i++) {
+//     console.log("salom")
+// }
+
+
+
+// const nameb = ["youtube", " Istagaram", "whatsap", "facebook"];
+
+
+// for (let i = 0; i < nameb.length; i++) {
+//     console.log(nameb[i]);
+// }
+
+
+
+// let a = prompt("gapingizni kirting");
+
+
+// if (a ==a) {
+//     console.log("yo'q bo'lmaydi");
+
+// } else {
+//     console.log("xa bo'ladi");
+
+// }
+
+
+
+
+
+// const joker = [a, b];
+
+// for (let i = 0; i < joker.length; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+
+// const users = ["Muhammadjon", "Muhammadsodiq", "Oyatilo", "muhammadhabib"];
+// console.log(users.length);
+
+// if (users.length > 3) {
+//     console.log("Bizda ismalr yetarli");
+// } else {
+//     console.log("ismlar kam");
+// }
+
+
+
+
+// const array = [false, true[1, 2], 12, true, "Execellent"];
+// console.log(typeof array[2]);
+
+
+// for (let i = 0; i < typeof array[i]; i++) {
+//     if (typeof array[1] == "string") {
+//         console.log(i);
+//     }
+// }
+
+
+
+234234
+
+
+
